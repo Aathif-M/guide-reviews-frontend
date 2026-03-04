7f043fec8643daa7c7a77db5a693771f0ece4564
Mohamed Aathif <aathifm99@gmail.com>
2026-03-05 02:10:15 +0530

    fix: import Link from react-router-dom in App.jsx to prevent render crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
 import { IconSun, IconMoon } from '@tabler/icons-react';
 
 import Home from './pages/Home';
